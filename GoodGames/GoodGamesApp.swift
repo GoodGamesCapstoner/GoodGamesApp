cf5600cdaffbab94509615de66f1451a3a6a6cbf
Jackson Secrist <jacksecrist@gmail.com>
2023-01-20 09:04:09 -0700

    Initial Commit

---
+++ GoodGames/GoodGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodGamesApp.swift
+//  GoodGames
+//
+//  Created by Jackson Secrist on 1/20/23.
+//
+
+import SwiftUI
+
+@main
+struct GoodGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

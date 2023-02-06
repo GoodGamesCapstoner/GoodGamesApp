7764fbd95d1a8a0186c859435fb0e4c362f9b708
Matt Goulding <matthewgoulding192@gmail.com>
2023-02-06 11:56:18 -0700

    Added emulator functionality in order to test offline and without messing up live data

--- GoodGames/GoodGamesApp.swift
+++ GoodGames/GoodGamesApp.swift
@@ -7,12 +7,40 @@
 
 import SwiftUI
 import FirebaseCore
+import FirebaseAuth
+import FirebaseStorage
+
+// EVENTUALLY I WILL NEED TO USE THIS CODE. TALK TO JACKSON ABOUT HOW TO ADAPT TO IT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
+      FirebaseApp.configure()
+#if EMULATORS
+      print(
+      """
+      *******************************
+      Testing on Emulators
+      *******************************
+      """
+      )
+      Auth.auth().useEmulator(withHost:"localhost", port:9099)
+      Storage.storage().useEmulator(withHost:"localhost", port:9199)
+      let settings = Firestore.firestore().settings
+      settings.host = "localhost:8080"
+      settings.isPersistenceEnabled = false
+      settings.isSSLEnabled = false
+      Firestore.firestore().settings = settings
+#elseif DEBUG
+      print(
+      """
+      *******************************
+      Testing on Live Server
+      *******************************
+      """
+      )
+#endif
+      return true
   }
 }
 

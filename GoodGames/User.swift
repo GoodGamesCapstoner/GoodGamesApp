7cca8088307905dc51c2ac00e74a4527670a02ad
Matt Goulding <matthewgoulding192@gmail.com>
2023-02-10 12:59:11 -0700

    Removed references and added files.

---
+++ GoodGames/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  GoodGames
+//
+//  Created by Matt Goulding on 2/10/23.
+//
+
+import Foundation
+
+/// The User object created when the user authenticates.
+public struct User: Codable {
+    let uid: String
+    let name: String
+    let email: String
+}
+

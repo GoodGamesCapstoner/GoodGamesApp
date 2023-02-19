c04f4c29b11456808ea04808808b086550e65df0
Matt Goulding <matthewgoulding192@gmail.com>
2023-02-19 09:48:25 -0700

    Image picker close to working. Starting to try and turn image into button now.

--- GoodGames/UserProfileView.swift
+++ GoodGames/UserProfileView.swift
@@ -18,12 +18,43 @@ struct UserProfileView: View {
                 .padding(.horizontal)
                 .padding(.top, 5)
             HStack {
-                AsyncImage(url: URL(string: Constants.profile_url)) { image in
-                    image.resizable() }
-                placeholder: {
-                        //nothin yet
+//                AsyncImage(url: URL(string: Constants.profile_url)) { image in
+//                    image.resizable() }
+//                placeholder: {
+//                        //nothin yet
+//                    }
+//                .frame(width: 180.0, height: 180.0)
+                if let image = vm.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .cornerRadius(50)
+                        .frame(width: 180, height: 180)
+                        .aspectRatio(contentMode: .fill)
+                        .clipShape(Circle())
+                } else {
+                    Circle()
+                        .frame(width: 200, height: 200)
+                        .foregroundColor(.black)
+                }
+                if vm.isUserAuthenticated == .signedIn {
+                        Button {
+                            vm.showSheet = true
+                        } label: {
+                            Text("Update Profile Image")
+                        }
+                        .buttonStyle(.borderedProminent)
+
+                }
+                    Spacer()
+                    .ignoresSafeArea()
+                    .padding(.horizontal, 20)
+                    .onChange(of: vm.image, perform: { image in
+                        vm.image = image
+                        vm.saveProfileImage()
+                    })
+                    .sheet(isPresented: $vm.showSheet) {
+                        ImagePicker(sourceType: .photoLibrary, selectedImage: self.$vm.image)
                     }
-                .frame(width: 180.0, height: 180.0)
                 VStack(alignment: .leading) {
                     VStack(alignment: .leading) {
                         Text("@AtomicBlonde14")
@@ -121,9 +152,9 @@ struct UserProfileView: View {
     }
 }
 
-fileprivate struct Constants {
-    static let profile_url: String = "https://wallpapers.com/images/hd/cute-retro-gaming-profile-xy3apbyc677bvcbl.jpg"
-}
+//fileprivate struct Constants {
+//    static let profile_url: String = "https://wallpapers.com/images/hd/cute-retro-gaming-profile-xy3apbyc677bvcbl.jpg"
+//}
 
 struct UserProfileView_Previews: PreviewProvider {
     static var previews: some View {
